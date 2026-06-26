35bc881d4f93575b5cbee9b75c796ab6e448fd9f
noerdmichel <50237329+noerdmichel@users.noreply.github.com>
2026-06-26 18:41:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app  = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 const HH_WFS = "https://geodienste.hamburg.de";
 
